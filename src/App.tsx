7f8a9f7c7421b75f8cef60320b6cb2ecb330f282
FierceBlizzard <43586417+FierceBlizzard@users.noreply.github.com>
2022-02-13 17:58:00 -0500

    Finished task 3 and got it working

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import "./App.css";
+import { Button, Col, Container, Row } from "react-bootstrap";
+import pfp from "C:/Users/abhik/tasks/src/assets/pfp.jpg";
 
 function App(): JSX.Element {
     return (
@@ -12,6 +14,27 @@ function App(): JSX.Element {
                 automatically reload. Iclyn
             </p>
             <p>Hello World</p>
+            <Button onClick={() => console.log("Hello World!")}>
+                Log Hello World
+            </Button>
+            <h1>New Header for task</h1>
+            <img src={pfp} alt="Me as a picrew" />
+            <br></br>
+            <div className="list-centering">
+                <ol>
+                    <li>The M2 pro max chip will be the best</li>
+                    <li>The new amd series chip</li>
+                    <li>Intel will last</li>
+                </ol>
+            </div>
+            <div className="rect">
+                <Container>
+                    <Row>
+                        <Col>First Column</Col>
+                        <Col>Second Column.</Col>
+                    </Row>
+                </Container>
+            </div>
         </div>
     );
 }
