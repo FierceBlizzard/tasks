5b779431ddc423fcbc9a7d800fcd5b816f32f235
Craig Barber <RacgiMan@icloud.com>
2022-02-14 10:56:10 -0500

    WORKING SHOUT FUNCTION!

--- src/functions.ts
+++ src/functions.ts
@@ -32,7 +32,8 @@ export function add3(first: number, second: number, third: number): number {
  * mark added to the end.
  */
 export function shout(message: string): string {
-    return "";
+    const shoutMessage = message.toUpperCase();
+    return shoutMessage + "!";
 }
 
 /**
