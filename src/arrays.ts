0337801cfae2aa4efca2b5509b66f2df03d1ac51
Craig Barber <RacgiMan@icloud.com>
2022-02-20 13:30:35 -0500

    Working stringToIntegers using a map with a ternary that check the result of parseInt, then parses again if true.

--- src/arrays.ts
+++ src/arrays.ts
@@ -32,7 +32,10 @@ export function tripleNumbers(numbers: number[]): number[] {
  * the number cannot be parsed as an integer, convert it to 0 instead.
  */
 export function stringsToIntegers(numbers: string[]): number[] {
-    return [];
+    const intArray = numbers.map((element: string): number =>
+        parseInt(element) ? parseInt(element) : 0
+    );
+    return intArray;
 }
 
 /**
