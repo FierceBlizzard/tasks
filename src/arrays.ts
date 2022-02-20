6024fda6d700c5fee26435bd1666fa2c9363afcc
Craig Barber <RacgiMan@icloud.com>
2022-02-20 13:15:16 -0500

    Functional triplednumbers function using a map!

--- src/arrays.ts
+++ src/arrays.ts
@@ -21,7 +21,10 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    const tripledNumbers = numbers.map(
+        (element: number): number => element * 3
+    );
+    return tripledNumbers;
 }
 
 /**
