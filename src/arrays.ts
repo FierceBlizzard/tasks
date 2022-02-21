357894500d39645f1fc1305cd091b92d7976ca83
FierceBlizzard <43586417+FierceBlizzard@users.noreply.github.com>
2022-02-21 12:57:03 -0500

    Wrote up tripleNumbers and got it working

--- src/arrays.ts
+++ src/arrays.ts
@@ -23,7 +23,8 @@ export function bookEndList(numbers: number[]): number[] {
  * number has been tripled (multiplied by 3).
  */
 export function tripleNumbers(numbers: number[]): number[] {
-    return numbers;
+    const triplenum = numbers.map((num: number): number => num * 3);
+    return triplenum;
 }
 
 /**
