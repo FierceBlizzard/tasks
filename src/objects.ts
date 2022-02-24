eee4dde408708c2eae3911e72640127ca5463596
FierceBlizzard <43586417+FierceBlizzard@users.noreply.github.com>
2022-02-23 19:33:07 -0500

    got the duplicateQuestion function working. Just have 2 more functions left to do

--- src/objects.ts
+++ src/objects.ts
@@ -94,8 +94,9 @@ export function toMarkdown(question: Question): string {
  * `newName`.
  */
 export function renameQuestion(question: Question, newName: string): Question {
-    question.name = newName;
-    return question;
+    const newQuestion = { ...question };
+    newQuestion.name = newName;
+    return newQuestion;
 }
 
 /**
@@ -104,7 +105,11 @@ export function renameQuestion(question: Question, newName: string): Question {
  * published; if it was published, now it should be not published.
  */
 export function publishQuestion(question: Question): Question {
-    return question;
+    const newQuestion = { ...question };
+    newQuestion.published === false
+        ? (newQuestion.published = true)
+        : (newQuestion.published = false);
+    return newQuestion;
 }
 
 /**
@@ -114,7 +119,11 @@ export function publishQuestion(question: Question): Question {
  * The `published` field should be reset to false.
  */
 export function duplicateQuestion(id: number, oldQuestion: Question): Question {
-    return oldQuestion;
+    const newQuestion = { ...oldQuestion };
+    newQuestion.id = id;
+    newQuestion.name = "Copy of " + newQuestion.name;
+    newQuestion.published = false;
+    return newQuestion;
 }
 
 /**
