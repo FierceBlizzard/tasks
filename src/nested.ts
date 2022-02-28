095ce95c0a56367a934035e114623a1fdc6b53c7
Craig Barber <RacgiMan@icloud.com>
2022-02-28 10:32:04 -0500

    Working getNonEmptyQuestions - Uses filter with a boolean condition making sure that the body & expected are empty strings, AND that the options array is of length 0 (empty)

--- src/nested.ts
+++ src/nested.ts
@@ -15,7 +15,14 @@ export function getPublishedQuestions(questions: Question[]): Question[] {
  * `expected`, and an empty array for its `options`.
  */
 export function getNonEmptyQuestions(questions: Question[]): Question[] {
-    return [];
+    return questions.filter(
+        (element: Question): boolean =>
+            !(
+                element.body === "" &&
+                element.expected === "" &&
+                element.options.length === 0
+            )
+    );
 }
 
 /***
