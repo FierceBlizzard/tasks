6c14052f3c36b803531dac98ba95ca344d0f3b45
Craig Barber <RacgiMan@icloud.com>
2022-03-09 17:31:22 -0500

    Work on checkAnswer.tsx - Merge issues, though? Working on a fix.

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,30 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
+type ChangeEvent = React.ChangeEvent<
+    HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+>;
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [answer, setAnswer] = useState<string>("");
+
+    function updateAnswer(event: ChangeEvent) {
+        setAnswer(event.target.value);
+    }
+
     return (
         <div>
             <h3>Check Answer</h3>
+            <Form.Group controlId="formAnswer">
+                <Form.Label>Answer:</Form.Label>
+                <Form.Control value={answer} onChange={updateAnswer} />
+                {answer === expectedAnswer && <span>✔️</span>}
+                {answer !== expectedAnswer && <span>❌</span>}
+            </Form.Group>
         </div>
     );
 }
