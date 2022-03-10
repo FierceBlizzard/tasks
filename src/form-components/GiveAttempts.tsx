2406991714c9b099e6434259d6aeee637b9e448f
Craig Barber <RacgiMan@icloud.com>
2022-03-09 21:49:11 -0500

    Working GiveAttempts.tsx - Has 2 useStates, one string and one number to handle all operations

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,43 @@
 import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+
+type ChangeTextEvent = React.ChangeEvent<
+    HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+>;
 
 export function GiveAttempts(): JSX.Element {
+    const [reqAttempts, setReqAttempts] = useState<string>("");
+
+    function changeReqAttempts(event: ChangeTextEvent) {
+        setReqAttempts(event.target.value);
+    }
+
+    const [numAttempts, setNumAttempts] = useState<number>(3);
+
+    function addNumAttempts() {
+        if (parseInt(reqAttempts) >= 0) {
+            setNumAttempts(numAttempts + parseInt(reqAttempts));
+        }
+    }
     return (
         <div>
             <h3>Give Attempts</h3>
+            <span>Number of attempts: {numAttempts} </span>
+            <Form.Group controlId="formReq">
+                <Form.Label>Requested Attempts?:</Form.Label>
+                <Form.Control
+                    type="number"
+                    value={reqAttempts}
+                    onChange={changeReqAttempts}
+                />
+                <Button onClick={addNumAttempts}>gain</Button>
+                <Button
+                    onClick={() => setNumAttempts(numAttempts - 1)}
+                    disabled={numAttempts <= 0}
+                >
+                    use
+                </Button>
+            </Form.Group>
         </div>
     );
 }
