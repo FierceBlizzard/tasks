d60442baff3c9f70da31795c8ffeeddfc3ff2d57
Craig Barber <RacgiMan@icloud.com>
2022-03-09 22:33:23 -0500

    Working ChangeColor.tsx - Uses map of constant array of COLORS to create radiobox with options. Selected color is track via string useState, and the current color is displayed below the radiobox options.

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,49 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
+const COLORS = [
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "cyan",
+    "blue",
+    "indigo",
+    "violet"
+];
 
 export function ChangeColor(): JSX.Element {
+    const [color, setColor] = useState<string>("red");
+
     return (
         <div>
             <h3>Change Color</h3>
+            {COLORS.map((colorElement: string) => (
+                <Form.Check
+                    key={colorElement}
+                    inline
+                    type="radio"
+                    name="colors"
+                    onChange={(event) => setColor(event.target.value)}
+                    id={"color-" + colorElement}
+                    label={colorElement}
+                    value={colorElement}
+                    style={{ backgroundColor: colorElement }}
+                    checked={color === colorElement}
+                />
+            ))}
+            <div>
+                <span>
+                    The current color is
+                    <span
+                        style={{ backgroundColor: color }}
+                        data-testid="colored-box"
+                    >
+                        {color}
+                    </span>
+                    !
+                </span>
+            </div>
         </div>
     );
 }
