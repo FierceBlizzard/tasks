399a8f6c0e22975b6741a6b4c2af0ee8ccb7f9f8
FierceBlizzard <43586417+FierceBlizzard@users.noreply.github.com>
2022-03-09 22:39:15 -0500

    Got reveal answer and change type working

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,6 +3,24 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [questType, setQuestionType] = useState<QuestionType>(
+        "short_answer_question"
+    );
+    function changeQuestionType(): void {
+        if (questType === "short_answer_question") {
+            setQuestionType("multiple_choice_question");
+        } else {
+            setQuestionType("short_answer_question");
+        }
+    }
+    return (
+        <div>
+            <Button onClick={changeQuestionType}>Change Type</Button>
+            {questType === "short_answer_question" && <div>Short Answer</div>}
+            {questType === "multiple_choice_question" && (
+                <div>Multiple Choice</div>
+            )}
+        </div>
+    );
     //this is to test if I got git finally working
 }
