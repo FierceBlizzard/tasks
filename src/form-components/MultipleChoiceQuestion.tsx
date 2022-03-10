404ebc8cd14e197eb6c164123b5df97ededb753a
Craig Barber <RacgiMan@icloud.com>
2022-03-09 22:47:57 -0500

    Working MultipleChoiceQuestion! - Uses map to populate the dropdown menu of selections, upon a selection updates the string state to match the select, displaying a check or a X based on if the selection option is correct or not.

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,9 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
+type ChangeEvent = React.ChangeEvent<
+    HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+>;
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +12,26 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    const [answer, setAnswer] = useState<string>(options[0]);
+
+    function updateAnswer(event: ChangeEvent) {
+        setAnswer(event.target.value);
+    }
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="choices">
+                <Form.Label>Multiple Choice Choices: </Form.Label>
+                <Form.Select value={answer} onChange={updateAnswer}>
+                    {options.map((option: string) => (
+                        <option key={option} value={option}>
+                            {option}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            {answer === expectedAnswer && <span>✔️</span>}
+            {answer !== expectedAnswer && <span>❌</span>}
         </div>
     );
 }
