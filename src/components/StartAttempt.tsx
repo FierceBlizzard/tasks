6db91c7600f021ebad058c27d0e363f44e09ec8f
FierceBlizzard <43586417+FierceBlizzard@users.noreply.github.com>
2022-03-10 00:12:29 -0500

    sleepy time. figure out startAttempt being weird

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,46 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [numAttempts, setNumAttempts] = useState<number>(4);
+    const [testing, setTest] = useState<boolean>(false);
+
+    function changeAttempts(numChange: number): void {
+        console.log("Attempt Num: ");
+        console.log(numAttempts + numChange + "\n");
+        setNumAttempts(numAttempts + numChange);
+    }
+    function changeTest(): void {
+        console.log("testing: ");
+        console.log(!testing + "\n");
+        setTest(!testing);
+    }
+    return (
+        <div>
+            <Button
+                onClick={() => {
+                    changeTest();
+                    changeAttempts(-1);
+                }}
+                disabled={testing || numAttempts === 0}
+            >
+                Start Quiz
+            </Button>
+            <Button
+                onClick={() => {
+                    changeTest();
+                }}
+                disabled={!testing}
+            >
+                Stop Quiz
+            </Button>
+            <Button
+                onClick={() => {
+                    changeAttempts(1);
+                }}
+                disabled={testing}
+            >
+                Mulligan
+            </Button>
+        </div>
+    );
 }
