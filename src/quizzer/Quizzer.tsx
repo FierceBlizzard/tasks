6de94166ea4597b0facf6a603fe09d2d77133ff3
Craig Barber <RacgiMan@icloud.com>
2022-03-16 19:48:05 -0400

    Added span which includes a list of sub-tasks I believe I've completed for grading purposes.

--- src/quizzer/Quizzer.tsx
+++ src/quizzer/Quizzer.tsx
@@ -4,6 +4,9 @@ export function Quizzer(): JSX.Element {
     return (
         <div>
             <h3>Quizzer</h3>
+            <div>
+                <span>Sub-Task 1, sketch</span>
+            </div>
             <img
                 src="https://i.imgur.com/Mv9xXLo.jpeg"
                 alt="A sketch of the Quizzer application"
