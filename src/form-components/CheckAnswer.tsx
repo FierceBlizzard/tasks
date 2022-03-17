f33347904e913b2fbfcfbf8d351565d3b28b7f21
FierceBlizzard <43586417+FierceBlizzard@users.noreply.github.com>
2022-03-17 14:48:19 -0400

    got checkAnswer working. Got a groove now

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -1,13 +1,28 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function CheckAnswer({
     expectedAnswer
 }: {
     expectedAnswer: string;
 }): JSX.Element {
+    const [givenAnswer, setAnswer] = useState<string>("");
+
+    function updateAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+        setAnswer(event.target.value);
+    }
     return (
         <div>
-            <h3>Check Answer</h3>
+            <h3>Check Answer:</h3>
+            <Form.Group controlId="short-answer-question">
+                <Form.Label>Answer:</Form.Label>
+                <Form.Control
+                    value={givenAnswer}
+                    onChange={updateAnswer}
+                ></Form.Control>
+                {expectedAnswer === givenAnswer && <span>✔️</span>}
+                {expectedAnswer !== givenAnswer && <span>❌</span>}
+            </Form.Group>
         </div>
     );
 }
