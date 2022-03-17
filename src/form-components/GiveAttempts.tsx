cd5a277216c69e450099aa0ad88eb470f9d3dd06
FierceBlizzard <43586417+FierceBlizzard@users.noreply.github.com>
2022-03-17 15:17:21 -0400

    got some work done on give attempts. Just need to code the 2 buttons

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,24 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
+    const [numAttempts, setAttempts] = useState<number>(3);
+    const [askAttempts, setAskAttempts] = useState<number>(0);
+
     return (
         <div>
             <h3>Give Attempts</h3>
+            {numAttempts}
+            <Form.Group controlId="attempt-form">
+                <Form.Label>Num Attempts:</Form.Label>
+                <Form.Control
+                    type="number"
+                    value={askAttempts}
+                    onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                        setAskAttempts(parseInt(event.target.value))
+                    }
+                ></Form.Control>
+            </Form.Group>
         </div>
     );
 }
