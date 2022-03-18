8e5b30637bfeeef95b2230a4d3eb015b08aa76de
FierceBlizzard <43586417+FierceBlizzard@users.noreply.github.com>
2022-03-17 23:23:28 -0400

    started on changeColor. Just need to figure out how to use map for making buttons. And then jsut the css stuff for acutal colors

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,36 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
+
+const COLORS = [
+    "red",
+    "blue",
+    "green",
+    "orange",
+    "purple",
+    "cyan",
+    "magenta",
+    "white",
+    "black"
+];
+const default_color = COLORS[0];
 
 export function ChangeColor(): JSX.Element {
+    const [curColor, setColor] = useState<string>(default_color);
     return (
         <div>
             <h3>Change Color</h3>
+            <Form.Check
+                inline
+                type="radio"
+                name="Colors"
+                value={curColor}
+                {COLORS.map((color: string) => 
+                    <option key={color} value={color} label={color}>
+                        {color}
+                    </option>
+                )}
+                onChange={(c) => setColor(c.target.value)}
+            />
         </div>
     );
 }
