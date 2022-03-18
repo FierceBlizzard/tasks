6d4d28d4cd55bf8e4b6fc0022e3948f9c3140ea9
FierceBlizzard <43586417+FierceBlizzard@users.noreply.github.com>
2022-03-18 14:00:45 -0400

    got a sketch

---
+++ src/quizzer/ListQuiz.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import sketch from "./assets/sketch.png";
+export function ListQuiz(): JSX.Element {
+    return <div></div>;
+}
--- src/quizzer/Quizzer.tsx
+++ src/quizzer/Quizzer.tsx
@@ -1,5 +1,17 @@
 import React from "react";
-
+import sketch from "./assets/sketch.jpg";
 export function Quizzer(): JSX.Element {
-    return <h3>Quizzer</h3>;
+    return (
+        <div>
+            <h3>List of questions</h3>
+            <div>
+                <span>Sketch</span>
+            </div>
+            <img
+                src={sketch}
+                alt="A sketch of the Quizzer application"
+                width="750px"
+            />
+        </div>
+    );
 }
